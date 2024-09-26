4d2d670633f1bd1a9e51be20d5987a0fbe0d03e4
asadkhokhar <asadkhokhar@cybertronlabs.com>
2024-09-26 14:32:49 +0500

    advertisement work

--- partials/header2.php
+++ partials/header2.php
@@ -1,10 +1,12 @@
 <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light inter">
-        <a class="navbar-brand pl-10" href="index.php">American English School</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="d-flex w-100 justify-content-between align-items-center">
+            <a class="navbar-brand pl-10" href="index.php"><img class="mw-10" src="assets/images/logo.jpeg" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav m-auto">
                 <li class="nav-item">
@@ -30,7 +32,7 @@
                 </li>
             </ul>
             <div class="inline-grid pr-15">
-                <a class="btn btn-primary me-2 m-b2" href="login">Login</a>
+                <a class="btn btn-primary me-2 " href="login">Login</a>
                 <a class="btn btn-primary" href="register">Register</a>
             </div>
         </div>
